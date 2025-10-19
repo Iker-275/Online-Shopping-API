40659d99fbbcd91f21df06527ac264bf51931bb2
Iker 275 <jeananderson275@gmail.com>
2025-10-20 03:33:54 +1000

    get products functional

--- controllers/products.js
+++ controllers/products.js
@@ -1,10 +1,20 @@
+const Product = require("../models/product")
+
+
 const getAllProducts = async (req,res)=> {
-    res.status(200).json({msg:"All products"});
+    const myData =  await Product.find();
+    res.status(200).json({ myData });
 }
 
+const getProductsByName = async (req,res)=> {
+    const myData =  await Product.find({name : 'iPhone 15 Pro' });
+    res.status(200).json({ myData });
+}
 const getAllProductsTesting = async (req,res)=> {
-    res.status(200).json({msg:"All products Testing"});
+    const testData =  await Product.find();
+
+    res.status(200).json({ testData });
     
 }
 
-module.exports ={getAllProducts,getAllProductsTesting}
+module.exports ={getAllProducts,getAllProductsTesting,getProductsByName}
--- routes/products.js
+++ routes/products.js
@@ -2,10 +2,13 @@ const express = require("express");
 
 const router = express.Router();
 
-const {getAllProducts,getAllProductsTesting} = require("../controllers/products");
+const {getAllProducts,getAllProductsTesting,getProductsByName} = require("../controllers/products");
 
 router.route("/").get(getAllProducts);
 router.route("/testing").get(getAllProductsTesting);
+router.route("/name").get(getProductsByName);
+
+
 
 
 module.exports = router;
