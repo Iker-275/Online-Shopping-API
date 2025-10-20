bb0aabe129ef0df732dbce2efd06bd3ba0fa9d75
Iker 275 <jeananderson275@gmail.com>
2025-10-20 21:39:46 +1000

    added regex to enable incomplete string searches

--- controllers/products.js
+++ controllers/products.js
@@ -7,7 +7,25 @@ const getAllProducts = async (req,res)=> {
 }
 
 const getProductsByName = async (req,res)=> {
-    const myData =  await Product.find( req.query);
+   const {company,name,featured,price} = req.query;
+   const queryObject = {};
+if(company)
+{
+    queryObject.company =  { $regex : company , $options: "i"};;
+}
+if(featured){
+    queryObject.featured = { $regex : featured , $options: "i"};
+}
+if(price){
+    queryObject.price =  price;
+}
+
+if(name){
+    queryObject.name = { $regex : name , $options: "i"};
+}
+
+//console.log(queryObject);
+    const myData =  await Product.find( queryObject);
     res.status(200).json({ myData });
 }
 const getAllProductsTesting = async (req,res)=> {
